28eb6adf62e71295deb4d43077269a0fb1d235a7
Etienne Goudot <etienne.goudot@hotmail.com>
2026-09-11 11:44:34 +0200

    mapManager: Save & restore of last position in map

--- lib/services/mapManager.dart
+++ lib/services/mapManager.dart
@@ -1,4 +1,5 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MapManager {
   Future<Position?> getCurrentPosition() async {
@@ -17,5 +18,31 @@ class MapManager {
     return await Geolocator.getCurrentPosition();
   }
 
+  Future<void> savePosition(
+      double latitude,
+      double longitude,
+      double zoom
+      ) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('latitude', latitude);
+    await prefs.setDouble('longitude', longitude);
+    await prefs.setDouble('zoom', zoom);
+  }
 
+  Future<Map<String, double>?> getLastSavePosition() async {
+    final prefs = await SharedPreferences.getInstance();
+    final latitude = prefs.getDouble('latitude');
+    final longitude = prefs.getDouble('longitude');
+    final zoom = prefs.getDouble('zoom');
+
+    if (latitude == null || longitude == null || zoom == null) {
+      return null;
+    }
+
+    return {
+      'latitude' : latitude,
+      'longitude' : longitude,
+      'zoom' : zoom,
+    };
+  }
 }
