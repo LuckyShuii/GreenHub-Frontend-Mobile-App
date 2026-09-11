b886c30e296bcfc8689ef087583c151fad9d37b1
Etienne Goudot <etienne.goudot@hotmail.com>
2026-09-11 12:34:46 +0200

    mapManager: add getPosition fn + return LatLng instead of double values

--- lib/services/mapManager.dart
+++ lib/services/mapManager.dart
@@ -1,8 +1,24 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MapManager {
-  Future<Position?> getCurrentPosition() async {
+  Future<Map<String, dynamic>?> getPosition() async {
+    final currentPosition = await getCurrentPosition();
+    if(currentPosition != null){
+      return currentPosition;
+    }
+    final lastSavePosition = await getLastSavePosition();
+    if(lastSavePosition != null){
+      return lastSavePosition;
+    }
+    return {
+      'latlang': LatLng(47.3941, 0.6848),
+      'zoom' : 15,
+    };
+  }
+
+  Future<Map<String, dynamic>?> getCurrentPosition() async {
     LocationPermission permission;
     permission = await Geolocator.checkPermission();
 
@@ -15,7 +31,11 @@ class MapManager {
       return null;
     }
 
-    return await Geolocator.getCurrentPosition();
+    Position position = await Geolocator.getCurrentPosition();
+    return {
+      'latlang': LatLng(position.latitude, position.longitude),
+      'zoom' : 15,
+    };
   }
 
   Future<void> savePosition(
@@ -29,7 +49,7 @@ class MapManager {
     await prefs.setDouble('zoom', zoom);
   }
 
-  Future<Map<String, double>?> getLastSavePosition() async {
+  Future<Map<String, dynamic>?> getLastSavePosition() async {
     final prefs = await SharedPreferences.getInstance();
     final latitude = prefs.getDouble('latitude');
     final longitude = prefs.getDouble('longitude');
@@ -40,8 +60,7 @@ class MapManager {
     }
 
     return {
-      'latitude' : latitude,
-      'longitude' : longitude,
+      'latlang': LatLng(latitude, longitude),
       'zoom' : zoom,
     };
   }
